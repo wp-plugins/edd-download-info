70fc08b9f9a1b5a8d96e38f7ceea85e6abd17c12
sami.keijonen <sami.keijonen@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-28 09:53:51 +0000

    Update edd download info to version 0.1.5
    git-svn-id: https://plugins.svn.wordpress.org/edd-download-info/trunk@747379 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- edd-download-info.php
+++ edd-download-info.php
@@ -3,7 +3,7 @@
 * Plugin Name: EDD Download Info
 * Plugin URI: https://foxnet-themes.fi/downloads/edd-download-info/
 * Description: Adds download info metabox and widget to Easy Digital Downloads.
-* Version: 0.1.4
+* Version: 0.1.5
 * Author: Sami Keijonen
 * Author URI: https://foxnet-themes.fi
 *
@@ -15,7 +15,7 @@
 * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 *
 * @package EDDDownloadInfo
-* @version 0.1.4
+* @version 0.1.5
 * @author Sami Keijonen <sami.keijonen@foxnet.fi>
 * @copyright Copyright (c) 2012, Sami Keijonen
 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
@@ -84,6 +84,7 @@ class EDD_Download_Info {
 		require_once( EDD_DOWNLOAD_INFO_INCLUDES . 'settings.php' );
 		require_once( EDD_DOWNLOAD_INFO_INCLUDES . 'shortcodes.php' );
 		require_once( EDD_DOWNLOAD_INFO_INCLUDES . 'scripts.php' );
+		require_once( EDD_DOWNLOAD_INFO_INCLUDES . 'functions.php' );
 		
 	}
 
